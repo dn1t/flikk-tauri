9ebfc4df61d97e3757d0740a67747531808f5517
dn1t <me@tica.fun>
2024-05-26 02:06:14 +0900

    feat: improved sidebar details

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <Sidebar />
-      <main class='w-full h-screen p-1.5'>
+      <main class='w-full h-screen pr-1.5 py-1.5'>
         <div class='w-full h-full bg-gray-1 rounded-lg'>
           <div class='px-4 py-3'>
             <h1>Hello, world!</h1>
--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -1,10 +1,18 @@
 export function Sidebar() {
   return (
-    <section class='w-52 h-screen px-4 pt-12' data-tauri-drag-region>
-      <div class='flex items-center gap-x-1'>
-        <img src='/flikk.png' alt='Flikk 로고' class='w-7 h-7' />
-        <h1 class='font-semibold text-2xl'>Flikk</h1>
+    <section class='w-60 h-screen'>
+      <div
+        class='flex items-center gap-x-1 px-4 pt-12 pb-2'
+        data-tauri-drag-region
+      >
+        <img
+          src='/flikk.png'
+          alt='Flikk 로고'
+          class='w-7 h-7 pointer-events-none'
+        />
+        <h1 class='font-semibold text-2xl pointer-events-none'>Flikk</h1>
       </div>
+      <div class='flex flex-col px-4'></div>
     </section>
   );
 }
