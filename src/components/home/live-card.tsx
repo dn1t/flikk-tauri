7a9427ba248788506f51f5a3ad232141b8693dbe
dn1t <me@tica.fun>
2024-06-08 19:14:23 +0900

    feat: added virtual scroll

---
+++ src/components/home/live-card.tsx
@@ -0,0 +1,103 @@
+import type { VirtualItem } from '@tanstack/solid-virtual';
+import { For } from 'solid-js';
+import type { Live } from '../../lib/types.d';
+import { ChzzkBadge, SOOPBadge } from '../common/badges';
+
+export function LiveCard(props: {
+  item: VirtualItem;
+  live: Live;
+  pos: 0 | 1 | 2;
+}) {
+  return (
+    <div
+      class='absolute w-[calc(33.333333%_-_13.333333px)]'
+      style={{
+        transform: `translateX(calc(${props.pos * 100}% + ${
+          props.pos * 20
+        }px)) translateY(${props.item.start}px)`,
+      }}
+    >
+      {props.live.thumbnail ? (
+        <>
+          <div
+            class='flex items-end gap-x-1 aspect-video px-[5px] py-1 bg-gray-2 bg-cover bg-no-repeat bg-center border border-gray-4 rounded-xl'
+            style={{ 'background-image': `url(${props.live.thumbnail})` }}
+          >
+            <div class='w-max flex items-center gap-x-1 px-1.5 py-1 bg-gray-2/70 backdrop-blur-sm rounded-md'>
+              <div class='w-1 h-1 bg-red-9 rounded-full' />
+              <div class='font-semibold text-xs text-red-9 leading-none line-clamp-2'>
+                {props.live.viewers.toLocaleString()}
+              </div>
+            </div>
+          </div>
+        </>
+      ) : props.live.adult ? (
+        <div class='flex flex-col items-center justify-center aspect-video bg-gray-2 border border-gray-4 rounded-xl'>
+          <div class='flex items-center justify-center w-[52px] aspect-square bg-gray-3 border-4 border-gray-5 rounded-full'>
+            <span class='font-medium text-gray-11 text-2xl leading-none'>
+              19
+            </span>
+          </div>
+          <span class='mt-1.5 font-semibold text-gray-10 text-lg'>
+            연령 제한
+          </span>
+        </div>
+      ) : (
+        <div class='flex flex-col items-center justify-center aspect-video bg-gray-2 border border-gray-4 rounded-xl'>
+          <span class='font-medium text-gray-10 text-lg'>이미지 없음</span>
+        </div>
+      )}
+      <div class='flex gap-x-2 mt-2.5'>
+        <div
+          class='flex-shrink-0 w-10 h-10 bg-cover bg-no-repeat bg-center border border-gray-4 rounded-full'
+          style={{
+            'background-image': `url(${props.live.channel.profileImage}), url(${
+              props.live.platform === 'chzzk'
+                ? 'https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na'
+                : props.live.platform === 'soop'
+                  ? 'https://res.afreecatv.com/images/afmain/img_thumb_profile.gif'
+                  : ''
+            })`,
+          }}
+        />
+        <div>
+          <div class='mt-0.5 font-medium hover:text-orange-10 leading-5 line-clamp-2 cursor-pointer'>
+            {props.live.title}
+          </div>
+          <div class='mt-1 font-medium text-[15px] text-gray-11 leading-[14px]'>
+            {props.live.channel.name}
+          </div>
+          <div class='flex flex-wrap gap-1 mt-2'>
+            {props.live.platform === 'chzzk' && (
+              <ChzzkBadge class='w-[22px] h-[22px] rounded-lg' />
+            )}
+            {props.live.platform === 'soop' && (
+              <SOOPBadge class='w-[22px] h-[22px] rounded-lg' />
+            )}
+            {props.live.platform === 'chzzk' && props.live.categoryLabel && (
+              <div class='px-1.5 py-1 bg-gray-4 font-medium text-xs text-gray-11 leading-none border border-gray-4 rounded-lg'>
+                {props.live.categoryLabel}
+              </div>
+            )}
+            {props.live.platform === 'soop' && (
+              <For each={props.live.categories}>
+                {(category) => (
+                  <div class='px-1.5 py-1 bg-gray-4 font-medium text-xs text-gray-11 leading-none border border-gray-4 rounded-lg'>
+                    {category}
+                  </div>
+                )}
+              </For>
+            )}
+            <For each={props.live.tags}>
+              {(tag) => (
+                <div class='px-1.5 py-1 bg-gray-2 font-medium text-xs text-gray-11 leading-none border border-gray-4 rounded-lg'>
+                  {tag}
+                </div>
+              )}
+            </For>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,7 @@
-import { Scrollbars } from 'solid-custom-scrollbars';
+import { createVirtualizer } from '@tanstack/solid-virtual';
 import { For, createEffect, createSignal, on } from 'solid-js';
-import { ChzzkBadge, SOOPBadge } from '../components/common/badges';
 import { Header } from '../components/common/header';
+import { LiveCard } from '../components/home/live-card';
 import { useFlikk } from '../context';
 import type { Live, SOOPLive } from '../lib/types.d';
 
@@ -11,6 +11,16 @@ export default function Home() {
     instances: { chzzk, soop },
   } = useFlikk();
   const [lives, setLives] = createSignal<Live[]>([]);
+  let listRef!: HTMLDivElement;
+
+  const virtualizer = createVirtualizer({
+    get count() {
+      return Math.ceil(lives().length / 3);
+    },
+    getScrollElement: () => listRef,
+    estimateSize: () => 305,
+    overscan: 5,
+  });
 
   createEffect(
     on(
@@ -19,7 +29,7 @@ export default function Home() {
         const [chzzkLivesRes, soopLivesRes] = await Promise.all([
           chzzk.getLives(),
           soop.getLives(),
-        ]); //300
+        ]);
         const result: Live[] = [];
 
         if (chzzkLivesRes.success) result.push(...chzzkLivesRes.data.lives);
@@ -63,112 +73,32 @@ export default function Home() {
     ),
   );
 
-  createEffect(() => {
-    console.log(lives().length);
-  });
-
   return (
-    <Scrollbars
-      class='max-h-64 h-full overflow-y-auto'
-      renderThumbVertical={(props) => <div {...props} class='bg-gray-6' />}
-      autoHide={true}
-    >
+    <div ref={listRef} class='h-full overflow-y-auto'>
       <Header title='홈' />
-      <div class='grid grid-cols-3 gap-5 px-8 mb-6'>
-        <For each={lives()}>
-          {(live) => {
-            return (
-              <div>
-                {live.thumbnail ? (
-                  <>
-                    <div
-                      class='flex items-end gap-x-1 aspect-video px-[5px] py-1 bg-gray-2 bg-cover bg-no-repeat bg-center border border-gray-4 rounded-xl'
-                      style={{ 'background-image': `url(${live.thumbnail})` }}
-                    >
-                      <div class='w-max flex items-center gap-x-1 px-1.5 py-1 bg-gray-2/70 backdrop-blur-sm rounded-md'>
-                        <div class='w-1 h-1 bg-red-9 rounded-full' />
-                        <div class='font-semibold text-xs text-red-9 leading-none line-clamp-2'>
-                          {live.viewers.toLocaleString()}
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                ) : live.adult ? (
-                  <div class='flex flex-col items-center justify-center aspect-video bg-gray-2 border border-gray-4 rounded-xl'>
-                    <div class='flex items-center justify-center w-[52px] aspect-square bg-gray-3 border-4 border-gray-5 rounded-full'>
-                      <span class='font-medium text-gray-11 text-2xl leading-none'>
-                        19
-                      </span>
-                    </div>
-                    <span class='mt-1.5 font-semibold text-gray-10 text-lg'>
-                      연령 제한
-                    </span>
-                  </div>
-                ) : (
-                  <div class='flex flex-col items-center justify-center aspect-video bg-gray-2 border border-gray-4 rounded-xl'>
-                    <span class='font-medium text-gray-10 text-lg'>
-                      이미지 없음
-                    </span>
-                  </div>
-                )}
-                <div class='flex gap-x-2 mt-2.5'>
-                  <div
-                    class='flex-shrink-0 w-10 h-10 bg-cover bg-no-repeat bg-center border border-gray-4 rounded-full'
-                    style={{
-                      'background-image': `url(${
-                        live.channel.profileImage
-                      }), url(${
-                        live.platform === 'chzzk'
-                          ? 'https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na'
-                          : live.platform === 'soop'
-                            ? 'https://res.afreecatv.com/images/afmain/img_thumb_profile.gif'
-                            : ''
-                      })`,
-                    }}
-                  />
-                  <div>
-                    <div class='mt-0.5 font-medium leading-5 line-clamp-2'>
-                      {live.title}
-                    </div>
-                    <div class='mt-1 font-medium text-[15px] text-gray-11 leading-[14px]'>
-                      {live.channel.name}
-                    </div>
-                    <div class='flex flex-wrap gap-1 mt-2'>
-                      {live.platform === 'chzzk' && (
-                        <ChzzkBadge class='w-[22px] h-[22px] rounded-lg' />
-                      )}
-                      {live.platform === 'soop' && (
-                        <SOOPBadge class='w-[22px] h-[22px] rounded-lg' />
-                      )}
-                      {live.platform === 'chzzk' && live.categoryLabel && (
-                        <div class='px-1.5 py-1 bg-gray-4 font-medium text-xs text-gray-11 leading-none border border-gray-4 rounded-lg'>
-                          {live.categoryLabel}
-                        </div>
-                      )}
-                      {live.platform === 'soop' && (
-                        <For each={live.categories}>
-                          {(category) => (
-                            <div class='px-1.5 py-1 bg-gray-4 font-medium text-xs text-gray-11 leading-none border border-gray-4 rounded-lg'>
-                              {category}
-                            </div>
-                          )}
-                        </For>
-                      )}
-                      <For each={live.tags}>
-                        {(tag) => (
-                          <div class='px-1.5 py-1 bg-gray-2 font-medium text-xs text-gray-11 leading-none border border-gray-4 rounded-lg'>
-                            {tag}
-                          </div>
-                        )}
-                      </For>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          }}
-        </For>
+      <div class='px-8 mb-6'>
+        <div
+          class='relative grid grid-cols-3 gap-5 w-full'
+          style={{ height: `${virtualizer.getTotalSize()}px` }}
+        >
+          <For each={virtualizer.getVirtualItems()}>
+            {(item) => {
+              const live1 = lives()[item.index * 3];
+              const live2 = lives()[item.index * 3 + 1];
+              const live3 = lives()[item.index * 3 + 2];
+              if (!live1 || !live2 || !live3) return;
+
+              return (
+                <>
+                  <LiveCard item={item} live={live1} pos={0} />
+                  <LiveCard item={item} live={live2} pos={1} />
+                  <LiveCard item={item} live={live3} pos={2} />
+                </>
+              );
+            }}
+          </For>
+        </div>
       </div>
-    </Scrollbars>
+    </div>
   );
 }
